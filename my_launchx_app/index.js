b08651e8a35c21abc804381d3a718cbfe9b71da7
FatimaGloria <janettgloria1@gmail.com>
2022-05-08 15:00:40 -0500

    3.2 Se crea archivo index donde se importa clase missionCommander

---
+++ my_launchx_app/index.js
@@ -0,0 +1,8 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const woopa = new MissionCommander("Woopa")
+const fatima = new MissionCommander("Fatima")
+
+console.log(carlo.name)
+console.log(woopa.name)
+console.log(fatima.name)
